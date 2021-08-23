9430d76f44185002508043d43eb9ce98e53e7597
João Carlos Dos Santos Pereira <joaocarloz@outlook.com.br>
2021-08-23 16:03:55 -0300

    Query Sql Alterado

--- QuerySQLOracle/OracleSQL01.sql
+++ QuerySQLOracle/OracleSQL01.sql
@@ -77,12 +77,13 @@ ORDER BY SALARY;
 
 
 
-
 /*Funcionarios que foram contratados no ultimo trimestre*/
 
 WITH 
   TAB_DADOS AS (SELECT FIRST_NAME||' '||LAST_NAME    AS NOME,
+
                        TO_CHAR(SALARY,'L999999D99')  AS SALARIO,
+                       
                        HIRE_DATE                     AS DATACON,            
                        DEPARTMENT_ID                 AS DEPARTAMENTO         
                   FROM EMPLOYEES),
